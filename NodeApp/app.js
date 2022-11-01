b2e510faa718648dc770586ee4f52b919475c8c4
itisaby <arnabbibhuti01@gmail.com>
2022-11-01 17:48:44 +0530

    Node app

--- NodeApp/app.js
+++ NodeApp/app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World! I am Arnab'));
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
