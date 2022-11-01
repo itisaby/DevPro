b5ca0b4f56ac10bd72cdd1bb39006c2843300941
itisaby <arnabbibhuti01@gmail.com>
2022-11-01 10:27:51 +0530

    Variables commented

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,15 +1,15 @@
-variable "region" {
-  type = string
-}
-variable "ami" {
-  type = string
-}
-variable "instance_type" {
-  type = string
-}
-variable "public_key" {
-  type = string
-}
-variable "key_name" {
-  type = string
-}
+# variable "region" {
+#   type = string
+# }
+# variable "ami" {
+#   type = string
+# }
+# variable "instance_type" {
+#   type = string
+# }
+# variable "public_key" {
+#   type = string
+# }
+# variable "key_name" {
+#   type = string
+# }
