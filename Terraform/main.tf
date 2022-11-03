5960bb3d0c9bafc7d72e9020f301fd7f4faed8e2
itisaby <arnabbibhuti01@gmail.com>
2022-11-03 16:22:55 +0530

    add subnet

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -18,60 +18,60 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_vpc" "main" {
- cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
-  enable_dns_support = true
-  instance_tenancy = "default"
- tags = {
-   Name = "Project VPC"
- }
+  enable_dns_support   = true
+  instance_tenancy     = "default"
+  tags = {
+    Name = "Project VPC"
+  }
 }
 resource "aws_subnet" "public_subnets" {
- count      = length(var.public_subnet_cidrs)
- vpc_id     = aws_vpc.main.id
- cidr_block = element(var.public_subnet_cidrs, count.index)
- availability_zone = element(var.azs, count.index)
- 
- tags = {
-   Name = "Public Subnet ${count.index + 1}"
- }
+  count             = length(var.public_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.public_subnet_cidrs, count.index)
+  availability_zone = element(var.azs, count.index)
+
+  tags = {
+    Name = "Public Subnet ${count.index + 1}"
+  }
 }
- 
+
 resource "aws_subnet" "private_subnets" {
- count      = length(var.private_subnet_cidrs)
- vpc_id     = aws_vpc.main.id
- cidr_block = element(var.private_subnet_cidrs, count.index)
- availability_zone = element(var.azs, count.index)
- tags = {
-   Name = "Private Subnet ${count.index + 1}"
- }
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.private_subnet_cidrs, count.index)
+  availability_zone = element(var.azs, count.index)
+  tags = {
+    Name = "Private Subnet ${count.index + 1}"
+  }
 }
 
 resource "aws_internet_gateway" "gw" {
- vpc_id = aws_vpc.main.id
- 
- tags = {
-   Name = "Project VPC IG"
- }
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "Project VPC IG"
+  }
 }
 
 resource "aws_route_table" "second_rt" {
- vpc_id = aws_vpc.main.id
- 
- route {
-   cidr_block = "0.0.0.0/0"
-   gateway_id = aws_internet_gateway.gw.id
- }
- 
- tags = {
-   Name = "2nd Route Table"
- }
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "2nd Route Table"
+  }
 }
 
 resource "aws_route_table_association" "public_subnet_asso" {
- count = length(var.public_subnet_cidrs)
- subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
- route_table_id = aws_route_table.second_rt.id
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+  route_table_id = aws_route_table.second_rt.id
 }
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
@@ -126,6 +126,7 @@ resource "aws_instance" "name" {
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   key_name               = aws_key_pair.deploy.key_name
   iam_instance_profile   = aws_iam_instance_profile.example.name
+  subnet_id              = aws_subnet.public_subnets[0].id
   connection {
     type        = "ssh"
     host        = self.public_ip
