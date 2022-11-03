11fcc508349aa954a882e49f1e8088520bc029a1
itisaby <arnabbibhuti01@gmail.com>
2022-11-03 16:12:15 +0530

    vpc public subnets created

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -17,20 +17,66 @@ provider "aws" {
   # Configuration options
   region = "us-east-1"
 }
-# resource "aws_vpc" "main" {
-#   cidr_block           = "10.123.0.0/16"
-#   instance_tenancy     = "default"
-#   enable_dns_hostnames = true
-#   enable_dns_support   = true
+resource "aws_vpc" "main" {
+ cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+  instance_tenancy = "default"
+ tags = {
+   Name = "Project VPC"
+ }
+}
+resource "aws_subnet" "public_subnets" {
+ count      = length(var.public_subnet_cidrs)
+ vpc_id     = aws_vpc.main.id
+ cidr_block = element(var.public_subnet_cidrs, count.index)
+ availability_zone = element(var.azs, count.index)
+ 
+ tags = {
+   Name = "Public Subnet ${count.index + 1}"
+ }
+}
+ 
+resource "aws_subnet" "private_subnets" {
+ count      = length(var.private_subnet_cidrs)
+ vpc_id     = aws_vpc.main.id
+ cidr_block = element(var.private_subnet_cidrs, count.index)
+ availability_zone = element(var.azs, count.index)
+ tags = {
+   Name = "Private Subnet ${count.index + 1}"
+ }
+}
 
-#   tags = {
-#     Name = "main"
-#   }
-# }
+resource "aws_internet_gateway" "gw" {
+ vpc_id = aws_vpc.main.id
+ 
+ tags = {
+   Name = "Project VPC IG"
+ }
+}
+
+resource "aws_route_table" "second_rt" {
+ vpc_id = aws_vpc.main.id
+ 
+ route {
+   cidr_block = "0.0.0.0/0"
+   gateway_id = aws_internet_gateway.gw.id
+ }
+ 
+ tags = {
+   Name = "2nd Route Table"
+ }
+}
+
+resource "aws_route_table_association" "public_subnet_asso" {
+ count = length(var.public_subnet_cidrs)
+ subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+ route_table_id = aws_route_table.second_rt.id
+}
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
-  # vpc_id      = aws_vpc.main.id
+  vpc_id      = aws_vpc.main.id
   ingress = [{
     description      = "egress"
     from_port        = 0
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -7,6 +7,22 @@
 # variable "instance_type" {
 #   type = string
 # }
+variable "public_subnet_cidrs" {
+  type        = list(string)
+  description = "Public Subnet CIDR values"
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  type        = list(string)
+  description = "Private Subnet CIDR values"
+  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+variable "azs" {
+  type        = list(string)
+  description = "Availability Zones"
+  default     = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+}
 variable "public_key" {
   type = string
 }
